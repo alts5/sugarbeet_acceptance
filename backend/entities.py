aa8364625e09e90ec0a19f94c6259402b3fb5c90
alts5 <133884186+alts5@users.noreply.github.com>
2024-10-20 17:53:58 +0300

    Добавлены сущности

--- backend/entities.py
+++ backend/entities.py
@@ -1,40 +1,45 @@
 from dataclasses import dataclass
 from enum import Enum
 
+
 class UserStatusEnum(Enum):
     BLOCK = 'Заблокирован'
     ACTIVE = 'Активен'
 
-#Классы-сущности
 @dataclass
 class User:
-    fio : str
-    login : str
-    password : str
-    stat : UserStatusEnum
+    fio: str
+    login: str
+    password: str
+    stat: UserStatusEnum
+
 
 @dataclass
 class Operator:
-    note : str
+    note: str
     user: User
+
 
 @dataclass
 class UnloadingOperator:
-    unload_place : str
-    note : str
+    unload_place: str
+    note: str
     user: User
+
 
 @dataclass
 class ScaleOperator:
-    primary_weight : float
-    secondary_weight : float
-    user : User
+    user: User
+    primary_weight: float = 0
+    secondary_weight: float = 0
+
 
 @dataclass
 class Laborant:
-    primary_check : str
-    secondary_check : str
-    user : User
+    primary_check: str
+    secondary_check: str
+    user: User
+
 
 @dataclass
 class TE:
@@ -42,23 +47,23 @@ class TE:
     sugarbeet_condition: str
     registration_number: str
     note: str
-    operator : Operator = None
-    unloading_operator : UnloadingOperator = None
-    scale_operator : ScaleOperator = None
-    laborant : Laborant = None
+    operator: Operator = None
+    unloading_operator: UnloadingOperator = None
+    scale_operator: ScaleOperator = None
+    laborant: Laborant = None
 
 @dataclass
 class AcceptingAct:
     te: TE
-    accept_info : str
-    weighting_result : float
+    accept_info: str
+    weighting_result: float = 0
 
 @dataclass
 class DistrReport:
-    te : TE
-    destination : str
+    te: TE
+    destination: str
 
 @dataclass
 class UnloadingReport:
-    te : TE
-    unload_info : str
+    te: TE
+    unload_info: str
