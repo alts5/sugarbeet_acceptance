088576aafc1ccee912a61f984dc7248fa03a9c45
alts5 <133884186+alts5@users.noreply.github.com>
2024-10-21 16:39:44 +0300

    Добавлен коннектор БД и чтение переменных окружения

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,35 @@
 from fastapi import *
 from starlette.middleware.cors import CORSMiddleware
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def mysql_query(query):
+    sql_return_data = set()
+    with mysql.connector.connect(
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
+        database=os.getenv('DB_NAME'),
+    ) as db:
+        cursor = db.cursor(dictionary=True) #Включает возврат в виде массива словарей
+        cursor.execute(query)
+        sql_return_data = cursor.fetchall()
+        db.commit()
+    return sql_return_data
+
 
 app = FastAPI()
-
 app.add_middleware(CORSMiddleware,
-               allow_origins=['http://localhost', 'http://127.0.0.1'],
+               allow_origins=['http://127.0.0.1', 'http://localhost'],
                allow_headers=['*'],
                allow_methods=['*'],
                allow_credentials=True)
 
 @app.post('/authenticate')
-def authenticate():
-    return 1
+def authenticate(user_login = Form(), user_password = Form()):
+    sql = mysql_query(f"SELECT id FROM usertbl WHERE login = '{user_login}' AND password = '(SELECT MD5({user_password}))'") #escape_string не нужен, делается автоматически
+    if sql
