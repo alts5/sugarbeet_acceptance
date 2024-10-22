5f187178bc6580ad9a5e0286a8b88f47d5a97188
alts5 <133884186+alts5@users.noreply.github.com>
2024-10-22 12:16:12 +0300

    Добавил идентификацию браузера

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,18 @@
+import hashlib
+from datetime import datetime
+from http import HTTPStatus
+
 from fastapi import *
+from passlib.handlers.md5_crypt import md5_crypt
 from starlette.middleware.cors import CORSMiddleware
 import mysql.connector
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def mysql_query(query):
-    sql_return_data = set()
     with mysql.connector.connect(
         user=os.getenv('DB_USER'),
         password=os.getenv('DB_PASSWORD'),
@@ -19,7 +24,11 @@ def mysql_query(query):
         cursor.execute(query)
         sql_return_data = cursor.fetchall()
         db.commit()
-    return sql_return_data
+    return sql_return_data or None
+
+def get_user(ubid: str):
+    temp_ubid = hashlib.md5(ubid.encode()).hexdigest()
+    return mysql_query(f"SELECT id FROM usertbl WHERE ubid = {temp_ubid}")
 
 
 app = FastAPI()
@@ -29,7 +38,15 @@ app.add_middleware(CORSMiddleware,
                allow_methods=['*'],
                allow_credentials=True)
 
+
 @app.post('/authenticate')
-def authenticate(user_login = Form(), user_password = Form()):
-    sql = mysql_query(f"SELECT id FROM usertbl WHERE login = '{user_login}' AND password = '(SELECT MD5({user_password}))'") #escape_string не нужен, делается автоматически
-    if sql
+def authenticate(request : Request, user_login = Form(), user_password = Form(), ubid = Body(embed=True)):
+    user = mysql_query(f"SELECT user FROM usertbl WHERE login = '{user_login}' AND password = (SELECT md5('{user_password}'))") #escape_string не нужен, делается автоматически
+    if user is None:
+        raise HTTPException(status_code=401, detail="Неверный логин или пароль")
+
+    ''' Если успешно прошли проверку'''
+    login_hash = hashlib.md5(ubid.encode()).hexdigest()
+    mysql_query(f"UPDATE usertbl SET ubid = '{login_hash}' WHERE login = '{user_login}'")
+
+@app.post('/')
